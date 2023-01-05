eb4a155cdb186485c5a34ee5b00b240fb106a000
topujss <adahmedt@gmail.com>
2023-01-05 15:17:46 -0500

    new project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './components/Header';
+import Home from './pages/Home/Home';
 
 export default function App() {
   return (
     <>
       <Header />
+      <Home />
     </>
   );
 }
