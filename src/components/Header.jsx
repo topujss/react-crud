ed973c1e573f55dc31f3bd23b08f1d66877390e3
topujss <adahmedt@gmail.com>
2023-01-05 15:13:57 -0500

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import { Component } from 'react';
+
+export default class Header extends Component {
+  render() {
+    return(
+      <h1 className="text-center">
+        <span>Eveyone Welcome</span> <b>&#10084;</b> to <strong>8 PM</strong> family
+      </h1>
+    )
+  }
+}
